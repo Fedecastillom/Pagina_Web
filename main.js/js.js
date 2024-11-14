3b101fb1c6b4132357dfacdcbf0737965907efd6
Matiasdiaz-sa <diazjm2005@gmail.com>
2024-11-13 21:08:47 -0300

    super commit pa

---
+++ main.js/js.js
@@ -0,0 +1,5 @@
+const menu = document.getElementById("menu")
+menu.addEventListener("click", ()=>{
+    document.querySelector(".navcontent").style.display = "flex";
+    document.querySelector(".gris").style.display = "flex";
+    })
